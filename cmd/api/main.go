bcbde64323dd0643aabc85cecba59ce52328dc84
Felipe Fernandes Versiane <felipeversiane09@gmail.com>
2024-08-19 21:05:38 -0300

    feat: configuring main file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/felipeversiane/task-api/internal/cache"
+	"github.com/felipeversiane/task-api/internal/database"
+	"github.com/felipeversiane/task-api/internal/log"
+)
+
+var (
+	port = os.Getenv("PORT")
+)
 
 func main() {
-	fmt.Sprintln("Hello World")
+	slog.Info("Configuring logs")
+	log.Configure()
+	slog.Info("Successfully configured logs")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	slog.Info("Configuring database")
+	if err := database.Connect(ctx); err != nil {
+		panic(err)
+	}
+	defer database.Close()
+	slog.Info("Successfully configured database")
+
+	slog.Info("Configuring cache")
+	if err := cache.Connect(); err != nil {
+		panic(err)
+	}
+	slog.Info("Successfully configured cache")
+
+	slog.Info("Running API")
+	go http.ListenAndServe(port, nil)
+
 }
